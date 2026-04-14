9982ca6231a38f47f9aa9cc8fa3d17a05287bead
Neufa <43496523+ulywae@users.noreply.github.com>
2026-04-14 13:11:38 +0700

    Delete build directory

--- build/core/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-C:\Users\ulywa\OneDrive\Desktop\makeLibESP\build\core\firmware_msc_fat.c.o: \
- C:\Users\ulywa\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\cores\esp32\firmware_msc_fat.c \
- C:\Users\ulywa\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\cores\esp32\firmware_msc_fat.h
--- build/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-C:\Users\ulywa\OneDrive\Desktop\makeLibESP\build\core\libb64\cencode.c.o: \
- C:\Users\ulywa\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\cores\esp32\libb64\cencode.c \
- C:\Users\ulywa\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\cores\esp32\libb64\cencode.h
